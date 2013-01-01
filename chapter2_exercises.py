8c25ce133322b2b35a14545ecc3cb793476d21fb
mrahmey <mrahmey@gmail.com>
2013-01-01 17:22:43 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,33 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Maurice Rahmey
+
+# 2.1: The numbers that start with a 0 are showing up wrong because the computer believes these are not binary numbers.
+
+# 2.2: The output when x = 5 and x + 1 is run as a script is 6. When a print statement is put before expressions, the outputs are 5 and 6.
+# print 5
+# x = 5
+# print x + 1
+
+# 2.3: 
+# 1) width/2 = 8 | int
+# 2) width/2.0 = 8.5 | float
+# 3) height/3 = 4.0 | float
+# 4) 1 + 2 * 5 = 11 | int
+# 5) delimiter * 5 = '.....' | str
+
+# 2.4:
+# 1) 523.3333333333334
+	# pi = 3.14
+	# r = 5.0
+	# 4.0/3.0*pi*r**3
+# 2) 945.45
+	# (24.95*0.6*60+0.75*(60-1)+3
+# 3) 7:30
+	# begin = (6*60+52)*60
+	# easy = (8*60+15)*2
+	# fast = (7*60+12)*3
+	# end = (begin + easy + fast)/(60*60.0)
+	# endHour = int(end)
+	# endMinute  = (end - endHour)*60
+	# print '%d:%d' % (endHour,endMinute)
